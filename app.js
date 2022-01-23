cb29fdbc602c126e8b287de8c9314f45f0d28d4d
rikhii20 <rikhisobari22@gmail.com>
2022-01-23 17:13:12 +0800

    + add cors

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const routes = require("./routes");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1", routes);
 
 app.listen(port, () => {
