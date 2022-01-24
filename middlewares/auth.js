f78fd138872570dc10fcb7d5f11f8643a9178fd8
rikhii20 <rikhisobari22@gmail.com>
2022-01-24 14:28:12 +0800

    + multer image only & file size

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,12 +13,12 @@ module.exports = {
         });
       }
       token = token.replace("Bearer ", "");
-      jwt.verify(token, process.env.SECRET_TOKEN, async(error, decoded) =>  {
+      jwt.verify(token, process.env.SECRET_TOKEN, async (error, decoded) => {
         if (error)
-        return res.status(401).json({
-          status: "Unauthorized",
-          message: "Invalid access token",
-        });
+          return res.status(401).json({
+            status: "Unauthorized",
+            message: "Invalid access token",
+          });
         const user = await Users.findOne({
           where: {
             id: decoded.id,
@@ -32,11 +32,10 @@ module.exports = {
         }
         req.user = {
           id: decoded.id,
-          email: decoded.email
-        }
+          email: decoded.email,
+        };
         next();
       });
-      
     } catch (error) {
       catchError(error, res);
     }
