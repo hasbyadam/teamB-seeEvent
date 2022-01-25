9d2e53e4d31e855586248ac7c82e3f47cae6b1f7
rikhii20 <rikhisobari22@gmail.com>
2022-01-25 12:18:31 +0800

    ! models event

--- models/event.js
+++ models/event.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Event.belongsTo(models.Users, {
-        as: "created by",
+        as: "createdBy",
         foreignKey: "user_id",
       });
       Event.belongsTo(models.Category, {
